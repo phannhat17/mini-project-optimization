10dddf537f6392d6531661604c28e54cf149eb76
phannhat17 <phandinhnhat1708@gmail.com>
2023-01-31 22:45:55 +0700

    update MIP

--- solver_file/MIP_model.py
+++ solver_file/MIP_model.py
@@ -55,7 +55,6 @@ if __name__ == '__main__':
     M = 1000000
 
     x = {} # x[(i,m)] = 1 iff item i is packed in car m else 0
-    z = {} # z[i] = m + 1 iff item i is packed in car m else nothing
     Ro = {} # if Ro = 1 then rotation = 90 degree, else 0
     l = {} # left coordination of item
     r = {} # right coordination of item
@@ -74,8 +73,6 @@ if __name__ == '__main__':
         solver.Add(r[i] == (1-Ro[i]) * data['size_item'][i][0] + Ro[i] * data['size_item'][i][1] + l[i])
         solver.Add(t[i] == (1-Ro[i]) * data['size_item'][i][1] + Ro[i] * data['size_item'][i][0] + b[i])
 
-        z[i] = solver.IntVar(1,k,f'z[{i}]')
-
         for m in range(k):
 
             x[(i,m)] = solver.IntVar(0, 1, 'x_[%i]_[%i]' %(i,m))
@@ -86,11 +83,6 @@ if __name__ == '__main__':
             solver.Add(t[i] <= (1-x[(i,m)]) * M + H_truck[m])
             solver.Add(b[i] <= (1-x[(i,m)]) * M + H_truck[m])    
 
-            # find the car that contain item i
-            solver.Add(z[i] <= (1-x[(i,m)]) * M + m +1)
-            solver.Add(z[i] >= x[(i,m)] * (m+1))
-            # z[i] = m+1 iff x[(i,m)] = 1
-
     for i in range(n):
         solver.Add(sum(x[(i,m)] for m in range(k)) == 1)
 
@@ -98,41 +90,26 @@ if __name__ == '__main__':
     # if 2 items is packed in the same car, they must be not overlaped
     for i in range(n - 1):
         for j in range(i + 1, n):
-            
-            constraints = [
-                (r[i], l[j]),
-                (r[j], l[i]),
-                (t[i], b[j]),
-                (t[j], b[i]),
-            ]
+            for m in range(k):
+                e = solver.IntVar(0, 1, f'e[{i}][{j}]')
+                solver.Add(e >= x[i,m] + x[j,m] - 1)
+                solver.Add(e <= x[i,m])
+                solver.Add(e <= x[j,m])
 
-            # c[s] = 1 => e <= f
-            # c[s] = 0 => e <= M + f => nothing
-            c = list() 
-            for s, (e,f) in enumerate(constraints):
-                c.append(solver.IntVar(0, 1, f'c[{i}][{j}][{s}]'))
-                solver.Add( e <= M * (1 - c[s]) + f)
-            # if z[i] == z[j] then d = 1
-            # else d == 0
+                # Binary variables for each constraint
+                c1 = solver.IntVar(0, 1, f'c1[{i}][{j}]')
+                c2 = solver.IntVar(0, 1, f'c2[{i}][{j}]')
+                c3 = solver.IntVar(0, 1, f'c3[{i}][{j}]')
+                c4 = solver.IntVar(0, 1, f'c4[{i}][{j}]')
+                
+                # Constraints that the binary variables must satisfy
+                solver.Add(r[i] <= l[j] + M * (1 - c1))
+                solver.Add(r[j] <= l[i] + M * (1 - c2))
+                solver.Add(t[i] <= b[j] + M * (1 - c3))
+                solver.Add(t[j] <= b[i] + M * (1 - c4))
 
-            # got: sum >= 0
-            # d = 1 => sum >= 1
-            # d = 0 => sum = 0
-            d = solver.IntVar(0, 1, f'b[{i}][{j}]')
-            solver.Add(sum(c) <= d * M)
-            solver.Add(sum(c) + (1 - d) * M >= 1)
-            
-            t0 = solver.IntVar(0, 1, f't0[{i}][{j}]')
-            t1 = solver.IntVar(0, 1, f't1[{i}][{j}]')
-            t2 = solver.IntVar(0, 1, f't2[{i}][{j}]')
-
-            # t0 = 0 => z[i] == z[j] => d = 1 => sum >=1
-            # t0 = 1 => z[i] != z[j] => d = 0 => sum = 0
-            solver.Add(z[j] + t0 <= z[i] + M * t1)
-            solver.Add(z[i] + t0 <= z[j] + M * t2)
-            solver.Add(t1 + t2 == t0)
-
-            solver.Add(d == 1 - t0)
+                solver.Add(c1 + c2 + c3 + c4 + (1-e)*M >= 1 )
+                solver.Add(c1 + c2 + c3 + c4 <= e*M )
 
     # find cars be used
     y = {} # y[m] = 1 iff car m be used
@@ -156,11 +133,12 @@ if __name__ == '__main__':
     solver.set_time_limit(time_limit * 1000)
 
     status = solver.Solve()
+    print(status)
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
         print('--------------Solution Found--------------')
         # for i in range(n):
         #     print(f'put item {i+1} with rotation {Ro[i].solution_value()} in car {z[i].solution_value()} at {l[i].solution_value()} {b[i].solution_value()} -> {r[i].solution_value()} {t[i].solution_value()}')
-        print(f'Number of bin used  :',len(set(z[i].solution_value() for i in range(n))))
+        print(f'Number of bin used  :',sum(y[m].solution_value() for m in range(k)))
         # print(f'bin be used         :',*set(z[i].solution_value() for i in range(n)))
         print(f'Total cost          : {solver.Objective().Value()}')
         print('----------------Statistics----------------')
@@ -171,5 +149,6 @@ if __name__ == '__main__':
         print(f'Time limit          : {time_limit}')
         print(f'Running time        : {solver.WallTime() / 1000}')
     
+
 
 
