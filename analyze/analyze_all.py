9709ef04885a8c34afd78f540a5055835177420f
phannhat17 <phandinhnhat1708@gmail.com>
2023-02-04 03:04:16 +0700

    new analyze

--- analyze/analyze_all.py
+++ analyze/analyze_all.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-resuslt_path = ["results/results_CP2/results_CP2_300_2.csv", "results/results_MIP/results_MIP_300_1.csv", "results/results_HEU/results_HEU_1.csv"]
+resuslt_path = ["results/results_CP1/results_CP1_300_2.csv", "results/results_CP2/results_CP2_300_2.csv", "results/results_MIP/results_MIP_300_1.csv", "results/results_HEU/results_HEU_1.csv"]
 
 success_tests = []
 fail_tests = []
@@ -33,47 +33,98 @@ for path in resuslt_path:
 
 
 # compare exact vs heuristics
-fig3, ax3 = plt.subplots(figsize=(15, 9))
-ax3.plot(n_packs[0], cost[0], "-", label = "CP2")
-ax3.plot(n_packs[2][:58], cost[2][:58], "-", label = "Heuristic")
-axins = ax3.inset_axes([0.55, 0.05, 0.4, 0.4])
+fig, ax = plt.subplots(figsize=(15, 9))
+ax.plot(n_packs[0], cost[0], "-", label = "CP1")
+ax.plot(n_packs[1], cost[1], "-", label = "CP2")
+ax.plot(n_packs[2], cost[2], "-", label = "MIP")
+ax.plot(n_packs[3][:58], cost[3][:58], "-", label = "Heuristic")
+axins = ax.inset_axes([0.55, 0.05, 0.4, 0.4])
 axins.plot(n_packs[0][:47], cost[0][:47], "-")
+axins.plot(n_packs[1][:47], cost[1][:47], "-")
 axins.plot(n_packs[2][:47], cost[2][:47], "-")
+axins.plot(n_packs[3][:47], cost[3][:47], "-")
 
 x1, x2, y1, y2 = 3, 61, 0, 630
 axins.set_xlim(x1, x2)
 axins.set_ylim(y1, y2)
 axins.set_xticklabels([])
 axins.set_yticklabels([])
-ax3.indicate_inset_zoom(axins)
+ax.indicate_inset_zoom(axins)
 
-ax3.set_ylabel('Total cost')
-ax3.set_xlabel('Number of packs')
+ax.set_ylabel('Total cost')
+ax.set_xlabel('Number of packs')
+
 plt.legend()
-plt.savefig('analyze/compare_cost_exact_heuristic.png')
+plt.savefig('analyze/compare_cost_all.png')
 
 
 # zoomed in n_packs < 100 compare exact vs heuristics
-fig4, ax4 = plt.subplots(figsize=(10, 6))
-ax4.plot(n_packs[0][:47], cost[0][:47], "-", label = "CP2")
-ax4.plot(n_packs[2][:47], cost[2][:47], "-", label = "Heuristic")
-
-ax4.set_ylabel('Total cost')
-ax4.set_xlabel('Number of packs')
+fig, ax = plt.subplots(figsize=(10, 10))
+ax.plot(n_packs[0][:47], cost[0][:47], "-", label = "CP1")
+ax.plot(n_packs[1][:47], cost[1][:47], "-", label = "CP2")
+ax.plot(n_packs[2][:47], cost[2][:47], "-", label = "MIP")
+ax.plot(n_packs[3][:47], cost[3][:47], "-", label = "Heuristic")
+ax.set_title('Zoom Exact vs Heuristics cost (lower is better)')
+ax.set_ylabel('Total cost')
+ax.set_xlabel('Number of packs')
 plt.legend()
-plt.savefig('analyze/zoomed_compare_cost_exact_heuristic.png')
+plt.savefig('analyze/zoomed_compare_cost_all.png')
 
 
 
+# 
+# 
+# 
+# compare CP vs heuristics
+fig, ax = plt.subplots(figsize=(15, 9))
+ax.plot(n_packs[0], cost[0], "-", label = "CP1")
+ax.plot(n_packs[1], cost[1], "-", label = "CP2")
+ax.plot(n_packs[3][:58], cost[3][:58], "-", label = "Heuristic")
+axins = ax.inset_axes([0.55, 0.05, 0.4, 0.4])
+axins.plot(n_packs[0][:47], cost[0][:47], "-")
+axins.plot(n_packs[1][:47], cost[1][:47], "-")
+axins.plot(n_packs[3][:47], cost[3][:47], "-")
+
+x1, x2, y1, y2 = 3, 61, 0, 630
+axins.set_xlim(x1, x2)
+axins.set_ylim(y1, y2)
+axins.set_xticklabels([])
+axins.set_yticklabels([])
+ax.indicate_inset_zoom(axins)
+
+ax.set_ylabel('Total cost')
+ax.set_xlabel('Number of packs')
+
+plt.legend()
+plt.savefig('analyze/compare_cost_CP_heuristic.png')
+
+
+# zoomed in n_packs < 100 compare CP vs heuristics
+fig, ax = plt.subplots(figsize=(15, 10))
+ax.plot(n_packs[0][:47], cost[0][:47], "-", label = "CP1")
+ax.plot(n_packs[1][:47], cost[1][:47], "-", label = "CP2")
+ax.plot(n_packs[3][:47], cost[3][:47], "-", label = "Heuristic")
+
+ax.set_ylabel('Total cost')
+ax.set_xlabel('Number of packs')
+plt.legend()
+plt.savefig('analyze/zoomed_compare_cost_CP_heuristic.png')
+
+
+
+# 
+# 
+# 
 # compare run time of fisrt 25 input
-fig5, ax5 = plt.subplots(figsize=(10,5))
-ax5.plot(n_packs[0][:25], run_time[0][:25], "-", label = "CP")
-ax5.plot(n_packs[1][:25], run_time[1][:25], "-", label = "MIP")
-ax5.plot(n_packs[2][:25], run_time[2][:25], "-", label = "Heuristic")
-ax5.set_ylabel('Run time (s)')
-ax5.set_xlabel('Number of packs')
+fig, ax = plt.subplots(figsize=(10,5))
+ax.plot(n_packs[0][:25], run_time[0][:25], "-", label = "CP1")
+ax.plot(n_packs[1][:25], run_time[1][:25], "-", label = "CP2")
+ax.plot(n_packs[2][:25], run_time[2][:25], "-", label = "MIP")
+ax.plot(n_packs[3][:25], run_time[3][:25], "-", label = "Heuristic")
+ax.set_ylabel('Run time (s)')
+ax.set_xlabel('Number of packs')
 plt.legend()
-plt.savefig('analyze/compare_first_25_run_time.png')
+plt.savefig('analyze/compare_run_time_all_first_25_test.png')
 
 
 
